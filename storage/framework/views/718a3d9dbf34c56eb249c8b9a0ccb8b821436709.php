4ec4e675d538b3a18f0fb9b5bf63e1c0a5183489
YesiS9 <yesisugandi900@gmail.com>
2025-02-10 09:19:39 +0700

    trying to fix some error while deploying on railways

--- storage/framework/views/718a3d9dbf34c56eb249c8b9a0ccb8b821436709.php
+++
@@ -1,2 +0,0 @@
-[<?php echo e($slot); ?>](<?php echo e($url); ?>)
-<?php /**PATH C:\Users\Administrator\Documents\web_development\Sentrad-main\SentradBe\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/header.blade.php ENDPATH**/ ?>
--- storage/framework/views/e74d5d6232755c82629801448a7574094d42a41d.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>
-
-<?php /**PATH C:\Users\Administrator\Documents\web_development\Sentrad-main\SentradBe\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/footer.blade.php ENDPATH**/ ?>
