ce86e4d18713227df7888af1e608966e40a0a58b
yesiS9 <yesisugandi900@gmail.com>
2025-09-28 01:28:27 +0700

    cors perbaikan

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,6 +21,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \Illuminate\Http\Middleware\HandleCors::class,
+        \App\Http\Middleware\CorsMiddleware::class,
     ];
 
     /**
